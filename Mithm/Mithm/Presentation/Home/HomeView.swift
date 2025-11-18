42293a7ceeff945a81e8d28cf318731ee6d62c7b
YunHak Lee <lyh990408@gmail.com>
2025-11-18 17:59:43 +0900

    [Setting] 기본적인 탭뷰 구성 추가

---
+++ Mithm/Mithm/Presentation/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Mithm
+//
+//  Created by YunhakLee on 11/18/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("HomeView")
+    }
+}
+
+#Preview {
+    HomeView()
+}
