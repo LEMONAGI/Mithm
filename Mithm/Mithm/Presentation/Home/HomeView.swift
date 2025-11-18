3ee491ecfe3f5e25e60e607b357d526957e20a36
YunHak Lee <lyh990408@gmail.com>
2025-11-18 21:37:11 +0900

    [Setting] 홈 화면 UI 간단하게 구현

--- Mithm/Mithm/Presentation/Home/HomeView.swift
+++ Mithm/Mithm/Presentation/Home/HomeView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var currentType: PhaseType = .luteal
     var body: some View {
-        Text("HomeView")
+        ZStack(alignment: .topTrailing) {
+            currentType.color
+                .ignoresSafeArea()
+            Image(currentType.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 402, height: 402/373*470)
+                .offset(y: 30)
+                
+            VStack(alignment: .leading, spacing: 0) {
+                Text("다음 월경")
+                    .bold()
+                Spacer().frame(height: 6)
+                Text("12월 18일 · D-23")
+                    .bold()
+                    .font(.title2)
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Text(currentType.name)
+                    .font(.system(size: 100, weight: .black, design: .monospaced))
+                Text(currentType.description)
+                    .bold()
+                Spacer()
+                Button {
+                    currentType = currentType.nextType
+                } label: {
+                    RoundedRectangle(cornerRadius: 30)
+                        .foregroundStyle(Color.black)
+                        .frame(height: 120)
+                        .overlay {
+                            Text("월경 시작")
+                                .foregroundStyle(.white)
+                                .bold()
+                                .font(.title)
+                        }
+                }
+                
+                Spacer()
+            }
+           .padding()
+        }
     }
 }
 
