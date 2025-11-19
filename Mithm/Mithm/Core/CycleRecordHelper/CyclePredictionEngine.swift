b61aaa6fb66eb488a5917d2c71141d7494a378ee
YunHak Lee <lyh990408@gmail.com>
2025-11-19 19:50:20 +0900

    [Chore] CyclePrediction Config 파일 해당 구조체 내부로 변경

--- Mithm/Mithm/Core/CycleRecordHelper/CyclePredictionEngine.swift
+++ Mithm/Mithm/Core/CycleRecordHelper/CyclePredictionEngine.swift
@@ -12,9 +12,21 @@ import Foundation
 
 struct CyclePredictionEngine {
     
-    let config: CyclePredictionConfig
+    struct Config {
+        /// 예측에 사용할 최소 사이클 개수 (이보다 적으면 예측 안 함)
+        let minimumCyclesForPrediction: Int
+        /// 미래에 몇 주기까지 예측할지
+        let numberOfFutureCycles: Int
+        
+        static let `default` = Config(
+            minimumCyclesForPrediction: 2,
+            numberOfFutureCycles: 3
+        )
+    }
+
+    let config: Config
     
-    init(config: CyclePredictionConfig = .default) {
+    init(config: Config = .default) {
         self.config = config
     }
     
